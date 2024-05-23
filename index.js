2701f66d129dd9af96a2f6391d2de48b033af374
Kurniawan Satria <satganzdevs@gmail.com>
2024-05-24 05:59:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
 import { fileURLToPath } from "url";
-import apirouter from './routes/api.js'
-
-
 
 
 const app = express();
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+
+import apirouter from './routes/api.js'
+
+
 app.enable('trust proxy');
 app.set("json spaces",2)
+app.use(morgan("dev"));
 app.use(cors())
-app.use('/api', apirouter)
 app.use(express.static(path.join(__dirname, "public")));
-
-
-
-
 
 
 // HOME
@@ -27,7 +28,9 @@ res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 
+app.use('/api', apirouter)
+
 // connected
 app.listen(3000, () => {
-console.log("CONNECTED TO http://localhost:3000");
+console.log("CONNECTED!");
 });
